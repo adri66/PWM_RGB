b081de170801bf73d8b25ed3db02bb81f15557ab
adri <adrienvandmme94@gmail.com>
2014-11-05 22:14:10 +0100

    Amélioration

--- pwn_rgb/pwn_rgb.cpp
+++ pwn_rgb/pwn_rgb.cpp
@@ -5,64 +5,70 @@
  *  Author: Adrien
  */ 
 
-#define  RED 5
-#define	 GREEN 6
+#define  RED 3
+#define	 GREEN 5
 #define  BLUE 11
-#define delay 100
+#define delay 10
 #define  F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-int r = 0, g = 0, b = 0;
 bool pwm(int duty_cicle, int arduino_pin)
 {
 	switch(arduino_pin)
 	{
 		case 3:
-		OCR2B = (duty_cicle*255)/100;
-		DDRD |= _BV(PIND3);
-		TCCR2A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
-		TCCR2B &= ~_BV(WGM02);
-		TCCR2B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
-		break;
-		case 11:
-		OCR2A = (duty_cicle*255)/100;
-		DDRB |= _BV(PINB3);
-		TCCR2A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
-		TCCR2B &= ~_BV(WGM02);
-		TCCR2B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+			OCR2B = (duty_cicle*255)/100;
+			DDRD |= _BV(PIND3);
+			TCCR2A |= (_BV(COM2B1)|_BV(COM2B0));
+			TCCR2A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+			TCCR2B &= ~_BV(WGM02);
+			TCCR2B = (TCCR2B&~(_BV(CS21)|_BV(CS22)))|_BV(CS20); //Fpwm = 62.5Khz
 		break;
 		case 5:
-		OCR0B = (duty_cicle*255)/100;
-		DDRD |= _BV(PIND5);
-		TCCR0A |= (_BV(COM0B1)|_BV(COM0B0));
-		TCCR0A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
-		TCCR0B &= ~_BV(WGM02);
-		TCCR0B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+			OCR0B = (duty_cicle*255)/100;
+			DDRD |= _BV(PIND5);
+			TCCR0A |= (_BV(COM0B1)|_BV(COM0B0));
+			TCCR0A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+			TCCR0B &= ~_BV(WGM02);
+			TCCR0B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
 		break;
 		case 6:
-		OCR0A = (duty_cicle*255)/100;
-		DDRD |= _BV(PINC6);
-		TCCR0A |= (_BV(COM0A1|_BV(COM0A0)));
-		TCCR0A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
-		TCCR0B &= ~_BV(WGM02);
-		TCCR0B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
-		break;
+			OCR0A = (duty_cicle*255)/100;
+			DDRD |= _BV(PIND6);
+			TCCR0A |= (_BV(COM0A1|_BV(COM0A0)));
+			TCCR0A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+			TCCR0B &= ~_BV(WGM02);
+			TCCR0B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+			break;
+		case 11:
+			OCR2A = (duty_cicle*255)/100;
+			DDRB |= _BV(PINB3);
+			TCCR2A |= (_BV(COM2A1)|_BV(COM2A0));
+			TCCR2A |= (_BV(WGM20)|_BV(WGM21)); //Fast PWM mode
+			TCCR2B &= ~_BV(WGM22);
+			TCCR2B = (TCCR2B&~(_BV(CS21)|_BV(CS22)))|_BV(CS20); //Fpwm = 62.5Khz
+			break;
 		default:
-		return false;
+			return false;
 	}
 	
 }
 int main(void)
 {
+	int r = 0, g = 0, b = 0;
     while(1)
     {
-		for (r=0; r<= 100 ; r++)
+		if(r==0)
 		{
-			pwm(100-r, RED);
-			pwm(100-g, GREEN);
-			pwm(100-b, BLUE);
-			_delay_ms(delay);
+			for (r=0; r<= 100 ; r++)
+			{
+				pwm(100-r, RED);
+				pwm(100-g, GREEN);
+				pwm(100-b, BLUE);
+				_delay_ms(delay);
+			}
 		}
+		r=100;
 		for (b=0; b<= 100 ; b++)
 		{
 			pwm(100-r, RED);
@@ -70,13 +76,15 @@ int main(void)
 			pwm(100-b, BLUE);
 			_delay_ms(delay);
 		}
-		for (r=100; r>= 0 ; r--)
+		b=100;
+		for (r=100; r >= 0 ; r--)
 		{
 			pwm(100-r, RED);
 			pwm(100-g, GREEN);
 			pwm(100-b, BLUE);
 			_delay_ms(delay);
 		}
+		r=0;
 		for (g=0; g<= 100 ; g++)
 		{
 			pwm(100-r, RED);
@@ -84,6 +92,7 @@ int main(void)
 			pwm(100-b, BLUE);
 			_delay_ms(delay);
 		}
+		g=100;
 		for (b=100; b>= 0 ; b--)
 		{
 			pwm(100-r, RED);
@@ -91,6 +100,7 @@ int main(void)
 			pwm(100-b, BLUE);
 			_delay_ms(delay);
 		}
+		b=0;
 		for (r=0; r<= 100 ; r++)
 		{
 			pwm(100-r, RED);
@@ -98,6 +108,7 @@ int main(void)
 			pwm(100-b, BLUE);
 			_delay_ms(delay);
 		}
+		r=100;
 		for (g=100; g >= 0 ; g--)
 		{
 			pwm(100-r, RED);
@@ -105,5 +116,6 @@ int main(void)
 			pwm(100-b, BLUE);
 			_delay_ms(delay);
 		}
+		g=0;
     }
 }

ad303dee54790d9ed7166f4cf6ba99784d772eee
adri <adrienvandmme94@gmail.com>
2014-11-05 20:58:59 +0100

    Premier commit

---
+++ pwn_rgb/pwn_rgb.cpp
@@ -0,0 +1,109 @@
+/*
+ * pwn_rgb.cpp
+ *
+ * Created: 5/11/2014 20:34:15
+ *  Author: Adrien
+ */ 
+
+#define  RED 5
+#define	 GREEN 6
+#define  BLUE 11
+#define delay 100
+#define  F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+int r = 0, g = 0, b = 0;
+bool pwm(int duty_cicle, int arduino_pin)
+{
+	switch(arduino_pin)
+	{
+		case 3:
+		OCR2B = (duty_cicle*255)/100;
+		DDRD |= _BV(PIND3);
+		TCCR2A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+		TCCR2B &= ~_BV(WGM02);
+		TCCR2B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+		break;
+		case 11:
+		OCR2A = (duty_cicle*255)/100;
+		DDRB |= _BV(PINB3);
+		TCCR2A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+		TCCR2B &= ~_BV(WGM02);
+		TCCR2B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+		break;
+		case 5:
+		OCR0B = (duty_cicle*255)/100;
+		DDRD |= _BV(PIND5);
+		TCCR0A |= (_BV(COM0B1)|_BV(COM0B0));
+		TCCR0A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+		TCCR0B &= ~_BV(WGM02);
+		TCCR0B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+		break;
+		case 6:
+		OCR0A = (duty_cicle*255)/100;
+		DDRD |= _BV(PINC6);
+		TCCR0A |= (_BV(COM0A1|_BV(COM0A0)));
+		TCCR0A |= (_BV(WGM00)|_BV(WGM01)); //Fast PWM mode
+		TCCR0B &= ~_BV(WGM02);
+		TCCR0B = (TCCR0B&~(_BV(CS01)|_BV(CS02)))|_BV(CS00); //Fpwm = 62.5Khz
+		break;
+		default:
+		return false;
+	}
+	
+}
+int main(void)
+{
+    while(1)
+    {
+		for (r=0; r<= 100 ; r++)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+		for (b=0; b<= 100 ; b++)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+		for (r=100; r>= 0 ; r--)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+		for (g=0; g<= 100 ; g++)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+		for (b=100; b>= 0 ; b--)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+		for (r=0; r<= 100 ; r++)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+		for (g=100; g >= 0 ; g--)
+		{
+			pwm(100-r, RED);
+			pwm(100-g, GREEN);
+			pwm(100-b, BLUE);
+			_delay_ms(delay);
+		}
+    }
+}
